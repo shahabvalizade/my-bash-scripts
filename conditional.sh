5980adbd51f156b5030836a46dcd688f5511ea09
Shahab Valizade <shahabvalizade@gmail.com>
2025-10-17 16:38:49 +0200

    files added

---
+++ conditional.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+read string1
+string2="value"
+if [ "$string1" = "$string2" ]
+then
+    echo 'yes'
+else
+    echo 'no'
+fi
+
---
+++ loop.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+read num
+for (( index=1 ; index<=$num ; index++ ))
+do
+    echo $index
+done
