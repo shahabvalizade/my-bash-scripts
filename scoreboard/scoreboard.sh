f78de40b33ffe3f6175fd50b99dfdec0abb25513
Shahab Valizade <shahabvalizade@gmail.com>
2025-10-18 20:14:58 +0200

    add validation to arguments add condition statements to awk

--- scoreboard/scoreboard.sh
+++ scoreboard/scoreboard.sh
@@ -17,7 +17,29 @@ while [[ $# -gt 0 ]]; do
   esac
 done
 
+if [[ -z "${username:-}" || -z "${addpoint:-}" ]]; then
+  echo "Usage: $0 --username <name> --addpoint <number>"
+  exit 1
+fi
+
+if ! [[ "$addpoint" =~ ^-?[0-9]+$ ]]; then
+  echo "Error: --addpoint must be an integer"
+  exit 1
+fi
+
 scores_file="scores.txt"
 touch $scores_file
 
-awk 'NF==2 { print $1, "has", $2, "score" }' scores.txt
+awk -v username="$username" -v addpoint="$addpoint" '
+  BEGIN { found=0 }
+  NF==2 {
+        if ($1 == username) { $2 = $2 + addpoint; found=1 }
+        print $1, $2
+        next
+  }
+  END {
+      if (!found) {
+          print username, 0 + addpoint
+      }
+  }
+' "scores.txt"
